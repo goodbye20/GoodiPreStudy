66c483dc3bdbbf9b74bd8132807b952a4372032b
Reload0213 <85924448+goodbye20@users.noreply.github.com>
2021-07-21 22:56:16 +0900

    StringExam

---
+++ GoodiPreStudy/src/sec2/EscapeExam.java
@@ -0,0 +1,12 @@
+package sec2;
+
+public class EscapeExam {
+	public static void main(String[] args) {
+		
+		System.out.println("번호\t이름\t직업");//탭만큼 뛰움
+		System.out.println("번호\n이름\n직업");//개행
+		System.out.println("번호\n이름\n직업");
+		System.out.println("우리는\"개발자\"입니다.");//""삽입
+		System.out.println("봄 \\ 여름 \\ 가을 \\ 겨울");// \삽입
+	}
+}
---
+++ GoodiPreStudy/src/sec2/FloatDoubleExam.java
@@ -0,0 +1,27 @@
+package sec2;
+
+public class FloatDoubleExam {
+	public static void main(String[] args) {
+		
+//		float var1 = 3.14;
+		double var2 = 3.14;
+		float var3 = 3.14f;
+		
+		System.out.println(var2);
+		System.out.println(var3);
+		
+		float var4 = 0.123456789f;
+		double var5 = 0.123456789;
+		
+		System.out.println(var4); //float 은 7자리까지
+		System.out.println(var5);
+		
+		double var6 = 3e6;
+		float var7 = 3e6f;
+		double var8 = 2e-3;
+		
+		System.out.println(var6);
+		System.out.println(var7);
+		System.out.println(var8);
+	}
+}
