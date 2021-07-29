8f4164958f381ff8fb77258f4d1e69e2ecab7503
Reload0213 <85924448+goodbye20@users.noreply.github.com>
2021-07-29 23:23:51 +0900

    Array study02

---
+++ GoodiPreStudy/src/sec6/exam03/ArrayReferenceObjactExample.java
@@ -0,0 +1,16 @@
+package sec6.exam03;
+
+public class ArrayReferenceObjactExample {
+	
+	public static void main(String[] args) {
+		
+		String[] strArray = new String[3];
+		strArray[0] = "java";
+		strArray[1] = "java";
+		strArray[2] = new String("java");
+		
+		System.out.println(strArray[0] == strArray[1]);
+		System.out.println(strArray[0] == strArray[2]);
+		System.out.println(strArray[0].equals(strArray[2]));
+	}
+}
